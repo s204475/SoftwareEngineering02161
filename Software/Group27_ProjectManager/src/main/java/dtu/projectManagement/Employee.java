126eca792c5e69a3748d5c31ade2124eaa82bd9a
Anders Gad <s204496@student.com>
2021-03-29 11:01:44 +0200

    Anders Gad employee class

---
+++ Software/Group27_ProjectManager/src/main/java/dtu/projectManagement/Employee.java
@@ -0,0 +1,52 @@
+package dtu.projectManagement;
+
+public class Employee {
+
+	private String name; 
+	
+	public Employee(String name) {
+		this.name=name;
+		
+	}
+	
+	public Project createProject(String title,int id) {
+		return Project(title,id);
+	}
+	
+	public void endProject() {
+		//will be implementet later.
+	}
+	
+	public void getSchedule() {
+		//will be implementet later.
+	}
+	
+	public Activity createActivity(String title, int duration) { //tilbagemelding fra Anders omkring, hvilken type, at duration har
+		return Activity(title,duration);
+	}
+	
+	public Activity editActivity() {
+		//will be implementet later.
+	}
+	
+	public void setHoursWorked() {
+		//will be implementet later
+	}
+	
+	public void editHoursWorked() {
+		//will be implementet later
+	}
+	
+	public void finishTask() {
+		//will be implementet later
+	}
+	
+	public boolean isAvabilble(timeSlot) {
+		return //activity in timeSlot == null ? true : false;
+	}
+	
+//	public Employee[] getAvalibleEmployees() { forkert sted, men rigtig struktur.
+//		List<Employee> avalibleEmplyess= new ArrayList<Employee>();
+//		for()
+//	}
+ }
