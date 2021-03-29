9d95c8d49ef313ad517e0e1d4239aba53fb9f8b0
ande310g <andersanders@live.dk>
2021-03-29 11:28:13 +0200

    update task duration added

--- Software/Group27_ProjectManager/src/main/java/dtu/projectManagement/Task.java
+++ Software/Group27_ProjectManager/src/main/java/dtu/projectManagement/Task.java
@@ -1,25 +1,30 @@
 package dtu.projectManagement;
 
+import java.time.Duration;
 import java.util.Calendar;
 
 public class Task {
 	private String name;
 	private DateServer dateServer = new DateServer();
 	private Calendar startTime;
-//	private Duration estimatedTime;
-//	private Duration timeSpent;
+	private Duration estimatedTime;
+	private Duration timeSpent;
 	
 	public Task (String name, Duration estimatedTime) {
 		this.name = name;
 		this.startTime = dateServer.getDate();
 		this.estimatedTime = estimatedTime;
-		
+		this.timeSpent = Duration.ofHours(0);
 		
 	}
 	
-//	public Duration getRemainingTime() {
-//		return estimatedTime - timeSpent;
-//	}
+	 public void setTimeSpent(Duration timeSpent) {
+		 this.timeSpent = this.timeSpent.plus(timeSpent);
+	 }
+	
+	public Duration getRemainingTime() {
+		return estimatedTime.minus(timeSpent);
+	}
 	
 	
 	
